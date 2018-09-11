586786c1eebdbe551bef770e5f7dd81edee052d9
Rachelslh <rachasalhi1999.rs@gmail.com>
2018-09-11 16:18:38 +0100

    Rectification De Design !

--- Module Gestion Inventaire/src/Presentation/Controller/DashBordController.java
+++ Module Gestion Inventaire/src/Presentation/Controller/DashBordController.java
@@ -1,22 +1,27 @@
 package Presentation.Controller;
 
 import Connections.ConnectionOrcl;
-import DAO.DAO;
+import DAO.*;
 import Exceptions.BuildingException;
 import Exceptions.NonExistantDansLaBDD;
 import Interfaces.Controller;
 import Presentation.FXML.Gestionaire_Interface;
 import Interfaces.Modele;
-
 import POJO.*;
 import Presentation.FXML.Main2;
 import Repository.*;
 import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXSpinner;
 import com.jfoenix.controls.JFXTextField;
+import javafx.concurrent.Service;
+import javafx.concurrent.Task;
+import javafx.concurrent.WorkerStateEvent;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.Parent;
@@ -35,13 +40,42 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.LinkedList;
 
-public class DashBordController extends Controller<Inventorier, Modele<Inventorier>> {
-    
-    
+public class DashBordController extends Controller<Inventorier, Modele<Inventorier>>  {
+
+
+    public JFXSpinner Spinner;
+    @FXML
+    private JFXComboBox<String> ChoixStockComboBox;
+
+    @FXML
+    private TextField NomStockCree;
+
+    @FXML
+    private TextField LocalisationStockCree;
+
+    @FXML
+    private ComboBox<String> InventaireUtilise;
+
+    @FXML
+    private CheckBox CompletionCheckbox;
+
+    @FXML
+    private CheckBox PrixCheckbox;
+
+    private HashMap<String,Integer> quantite = new HashMap<>();
+    private Stage PS = new Stage();
+    private boolean visuel_ready = false;
+    private Service<Void> chargement_en_cours;
+
     public void ChoixStock(ActionEvent actionEvent) {
+        visuel_ready = false;
+        chargement_en_cours = chargement_bdd_task(ChoixStockComboBox.getValue());
+        chargement_en_cours.start();
     }
 
     public void AjouterStockButton(ActionEvent actionEvent) {
@@ -50,19 +84,61 @@ public class DashBordController extends Controller<Inventorier, Modele<Inventori
     public void AjoutPlusieursTitres(ActionEvent actionEvent) {
     }
 
-    public void VerifierButton(ActionEvent actionEvent) {
+    public void VerifierButton(ActionEvent actionEvent) throws IOException, SQLException{
+        Stage PS = new Stage();
+        PS.initStyle(StageStyle.TRANSPARENT);
+        FXMLLoader loader = new FXMLLoader ( );
+        loader.setLocation ( Main2.class.getResource ( "Verification_Panel.fxml" ) );
+        Parent root = loader.load ( );
+        Verification_Panel_Controller FOP = loader.getController ( );
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        GUI.moveStage(PS,root);
+        FOP.setStockEtInventaire(ChoixStockComboBox.getValue(),InventaireUtilise.getValue());
+        PS.setScene(scene);
+        PS.show ( );
     }
 
-    public void AdditionnerButton(ActionEvent actionEvent) {
+    public void AdditionnerButton(ActionEvent actionEvent) throws IOException, SQLException{
+        Stage PS = new Stage();
+        PS.initStyle(StageStyle.TRANSPARENT);
+        FXMLLoader loader = new FXMLLoader ( );
+        loader.setLocation ( Main2.class.getResource ( "Addition_Panel.fxml" ) );
+        Parent root = loader.load ( );
+        GUI.moveStage(PS,root);
+        Correction_Panel FOP = loader.getController ( );
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        FOP.setStockEtInventaire(ChoixStockComboBox.getValue(),InventaireUtilise.getValue());
+        PS.setScene(scene);
+        PS.show ( );
     }
 
     public void TransfererButton(ActionEvent actionEvent) {
     }
 
-    public void SoustraireButton(ActionEvent actionEvent) {
+    public void SoustraireButton(ActionEvent actionEvent) throws IOException, SQLException {
+        Stage PS = new Stage();
+        PS.initStyle(StageStyle.TRANSPARENT);
+        FXMLLoader loader = new FXMLLoader ( );
+        loader.setLocation ( Main2.class.getResource ( "Soustraction_Panel.fxml" ) );
+        Parent root = loader.load ( );
+        GUI.moveStage(PS,root);
+        Correction_Panel FOP = loader.getController ( );
+        Scene scene = new Scene(root);
+        scene.setFill(Color.TRANSPARENT);
+        FOP.setStockEtInventaire(ChoixStockComboBox.getValue(),InventaireUtilise.getValue());
+        PS.setScene(scene);
+        PS.show ( );
     }
 
-    public void CreerAction(ActionEvent actionEvent) {
+    public void CreerAction(ActionEvent event) throws SQLException, IllegalAccessException {
+        Stock S = new Stock();
+        S.setNom(NomStockCree.getText());
+        S.setLocalisation(LocalisationStockCree.getText());
+        S.setDate_realisation(Calendar.getInstance().getTime());
+        DAO<Stock> DS = new StockDAO(new Stock());
+        DS.ajouter(S);
     }
 
     //CORRECTIFS ERREURS 
@@ -220,6 +296,16 @@ public class DashBordController extends Controller<Inventorier, Modele<Inventori
     void initialize() throws SQLException, IllegalAccessException, NonExistantDansLaBDD, BuildingException {
         clean_interface();
         load_interface();
+        Statement S = ConnectionOrcl.getInstance().createStatement();
+        ResultSet RS = S.executeQuery("SELECT  NOM_S FROM STOCK");
+        ChoixStockComboBox.getItems().clear();
+        while (RS.next()){
+            ChoixStockComboBox.getItems().add(RS.getString(1));
+        }
+        RS = S.executeQuery("SELECT NOM_INVENTAIRE FROM INVENTAIRES WHERE FINALISE='true'");
+        while(RS.next()){
+            InventaireUtilise.getItems().add(RS.getString(1));
+        }
     }
 
 
@@ -359,5 +445,85 @@ public class DashBordController extends Controller<Inventorier, Modele<Inventori
         }
     }
 
+    //GestionStockController :
+
+    public Service<Void> chargement_bdd_task(String nom){
+        return  new Service<Void>() {
+            @Override
+            protected Task<Void> createTask() {
+                Task<Void> T = new Task<Void>(){
+                    @Override
+                    protected Void call() {
+                        System.out.println("Début du chargement");
+                        PS.initStyle(StageStyle.TRANSPARENT);
+                        FXMLLoader loader = new FXMLLoader ( );
+                        loader.setLocation ( Main2.class.getResource ( "Visuel_Stock.fxml" ) );
+                        Parent root = null;
+                        try {
+                            root = loader.load ( );
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        GUI.moveStage(PS,root);
+                        Controller_Visuel_Stock FOP = loader.getController ( );
+                        Scene scene = new Scene(root);
+                        scene.setFill(Color.TRANSPARENT);
+                        try {
+                            FOP.setNom_stock(nom);
+                        } catch (SQLException | BuildingException | NonExistantDansLaBDD | IllegalAccessException e) {
+                            e.printStackTrace();
+                        }
+                        try {
+                            PS.setScene(scene);
+                        }catch(Exception e){
+                            System.out.println(e.getMessage());
+                        }
+                        Spinner.setVisible(false);
+                        System.out.println("Fin du chargement");
+
+                        return null;
+                    }
+                };
+                T.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+                    @Override
+                    public void handle(WorkerStateEvent event) {
+                        visuel_ready = true;
+                    }
+                });
+                T.setOnFailed(new EventHandler<WorkerStateEvent>() {
+                    @Override
+                    public void handle(WorkerStateEvent event) {
+                        reset();
+                    }
+                });
+                return T;
+
+            }
+        };
+    }
+
+    @FXML
+    public void Gerer_Stock_Action(ActionEvent actionEvent) throws IOException, SQLException, BuildingException, IllegalAccessException, NonExistantDansLaBDD {
+        if(visuel_ready) {
+            Stage s= (Stage) acceuilPane.getScene().getWindow();
+            s.hide();
+            PS.show();
+        }
+        else
+        {
+            chargement_en_cours.setOnSucceeded(new EventHandler<WorkerStateEvent>() {
+                @Override
+                public void handle(WorkerStateEvent event) {
+                    PS.show();
+                }
+            });
+            if(!chargement_en_cours.isRunning())
+                chargement_en_cours.start();
+        }
+    }
+
+    public void Supprimer_Stock(ActionEvent actionEvent) {
+
+    }
 }
 
